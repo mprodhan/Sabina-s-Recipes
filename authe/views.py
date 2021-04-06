298ae8feccb6bbb0323a62580427d7b9e1c1d6cb
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2021-04-06 00:12:02 -0400

    created forms for login and signup.

--- authe/views.py
+++ authe/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect, \
+    HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
