5efa2f0c6d353654b31a0030906f84a74130747b
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2021-04-05 23:22:46 -0400

    setup the settings and urls configuration .

---
+++ recipe_profile/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RecipeProfileConfig(AppConfig):
+    name = 'recipe_profile'
---
+++ recipe_profile/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from recipe_profile import views
+
+urlpatterns = [
+    
+]
